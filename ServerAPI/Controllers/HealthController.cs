d5895b2c564f0ff139019df56ef9f8d45f2558e0
rrjiho <rjiho0125@naver.com>
2025-06-17 17:33:08 +0900

    add HealthController.cs modify docker-compose.yml

---
+++ ServerAPI/Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServerAPI.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("Healthy");
+    }
+}
